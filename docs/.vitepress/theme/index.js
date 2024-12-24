51257c80d5922d19e2cacc0c36f94899b849ae06
CHEN WEI JUN <141419683+helpfulcraft@users.noreply.github.com>
2024-12-24 19:30:15 +0800

    fix: conditionally use editor layout

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,5 @@
 // .vitepress/theme/index.js
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import HeadingColors from './HeadingColors.vue'
@@ -9,5 +10,11 @@ export default {
   enhanceApp({ app }) {
     app.component('HeadingColors', HeadingColors)
   },
-  Layout: EditorLayout
+  Layout: () => {
+    // 检查当前路径是否是编辑器页面
+    if (typeof window !== 'undefined' && window.location.pathname.includes('/editor')) {
+      return h(EditorLayout)
+    }
+    return h(DefaultTheme.Layout)
+  }
 }
